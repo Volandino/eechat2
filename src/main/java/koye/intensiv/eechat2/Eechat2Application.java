eed7b0a2b2b8acdd9e9b57b1830824da9330bc0c
yermek <ye.koishibekov@niikmg.kz>
2021-05-22 16:26:33 +0600

    'initial'

---
+++ src/main/java/koye/intensiv/eechat2/Eechat2Application.java
@@ -0,0 +1,13 @@
+package koye.intensiv.eechat2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Eechat2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Eechat2Application.class, args);
+    }
+
+}
---
+++ src/test/java/koye/intensiv/eechat2/Eechat2ApplicationTests.java
@@ -0,0 +1,13 @@
+package koye.intensiv.eechat2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Eechat2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
